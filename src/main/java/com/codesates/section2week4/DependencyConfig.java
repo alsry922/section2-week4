b83bcb7b3333c6d1c811cb3bd9e182859e45c256
alsry922 <alsry922@gmail.com>
2022-10-13 16:41:04 +0900

    스프링 컨테이너를 사용한 싱글톤 예제 실습

--- src/main/java/com/codesates/section2week4/DependencyConfig.java
+++ src/main/java/com/codesates/section2week4/DependencyConfig.java
@@ -4,19 +4,27 @@ import com.codesates.section2week4.coffee.CoffeeRepository;
 import com.codesates.section2week4.coffee.CoffeeService;
 import com.codesates.section2week4.member.MemberRepository;
 import com.codesates.section2week4.member.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DependencyConfig {
-  public MemberService memberService(){
+  @Bean
+  public MemberService memberService() {
     return new MemberService(memberRepository());
   }
+
+  @Bean
   public MemberRepository memberRepository() {
     return new MemberRepository();
   }
 
+  @Bean
   public CoffeeService coffeeService() {
     return new CoffeeService(coffeeRepository());
   }
 
+  @Bean
   public CoffeeRepository coffeeRepository() {
     return new CoffeeRepository();
   }
